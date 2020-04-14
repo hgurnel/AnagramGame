545547e17287c47358a76e1afa7c58140e3db98b
hgurnel <hgurnel@gmail.com>
2020-04-14 19:04:19 +0200

    Update comments

--- anagramGame.h
+++ anagramGame.h
@@ -11,25 +11,22 @@ public:
 	AnagramGame(const string& word);
 	virtual ~AnagramGame() {}
 
-	/// <summary>
-	/// Submit a word on behalf of a user. A word is accepted if its letters are 
-	/// contained in the original string submitted in the constructor, and if it 
-	/// is in the word list at https://sites.google.com/a/ifyoucan.org/downloads/downloads/wordlist.txt. 
-	/// If the word is accepted and its score is high enough, the submission 
-	/// should be added to the high score list. If there are multiple submissions 
-	/// with the same score, all are accepted, but the first submission with that 
-	/// score should rank higher. 
-	/// </summary>
-	/// <param name="word">Word submitted by the user</param>
+	/*Submit a word on behalf of a user. A word is accepted if its letters are 
+	contained in the original string submitted in the constructor (anagramWord), 
+	and if it is in the word list at 
+	https://sites.google.com/a/ifyoucan.org/downloads/downloads/wordlist.txt. 
+	If the user word is accepted, the submission should be added to the high score 
+	list. If there are multiple submissions with the same score, all are accepted.
+	Param name="word": word submitted by the user*/
 	void SubmitWord(string &word);
 
-	// Check if submittedWord is a substring of anagramWord
+	// Check if the submitted word (userWord) is a substring of anagramWord
 	bool IsUserWordValid(const string &userWord, const string &anagramWord);
 
-	// Check if string 1 is a substring of string 2 (no order)
+	// Check if string 1 is a substring of string 2 (in any order of the characters)
 	bool IsSubstring(const string &s1, const string &s2);
 
-	// Check if submittedWord belongs to the word list
+	// Check if submittedWord belongs to the word list from Google
 	bool IsAnagram(const string &filePath);
 
 	// Update the array containing the best scores
